32841c79946917aa30ba062a3a0604d4e5a71430
Simon Dutka <SimonDutkaWork@gmail.com>
2022-03-29 15:24:52 +0200

    feat: added navbar component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './app.scss';
+
+// Components
+import { Navbar } from './components';
+
+// Pages
+import {} from './pages';
+
+const app = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
+
+export default app;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import './navbar.scss';
+
+// Images
+import logo from '../../assets/logo.svg';
+
+const Logo = () => (
+    <>
+        <img className='navbar__menu-logo' src={logo} alt='payme logo' />
+    </>
+);
+
+const Menu = () => (
+    <>
+        <ul className='navbar__menu-container'>
+            <li className='navbar__menu-element'>
+                <a href='#home'>Desitnations</a>
+            </li>
+            <li className='navbar__menu-element'>
+                <a href='#about'>Hotels</a>
+            </li>
+            <li className='navbar__menu-element'>
+                <a href='#testimonials'>Flights</a>
+            </li>
+            <li className='navbar__menu-element'>
+                <a href='#contact'>Bookings</a>
+            </li>
+        </ul>
+    </>
+);
+
+const Sign = () => (
+    <>
+        <div className='navbar__btn-container'>
+            <button type='submit' className='navbar__btn'>
+                Login
+            </button>
+            <button type='submit' className='navbar__btn navbar__btn--sign-up'>
+                Sign up
+            </button>
+        </div>
+    </>
+);
+
+const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    return (
+        <div className='navbar'>
+            {openMenu ? (
+                <>
+                    <div className='navbar--small'>
+                        <Logo />
+
+                        <div
+                            className='navbar__menu-hamburger-container'
+                            onClick={() => setOpenMenu(false)}
+                        >
+                            <div
+                                className='navbar__menu-hamburger navbar__menu-hamburger--open'
+                                onClick={() => setOpenMenu(false)}
+                            ></div>
+                        </div>
+                        <div className='navbar__menu--large'>
+                            <Menu />
+                        </div>
+                    </div>
+
+                    <div className='navbar__menu--small'>
+                        <Menu />
+                    </div>
+
+                    <Sign />
+                </>
+            ) : (
+                <>
+                    <div className='navbar--small'>
+                        <Logo />
+
+                        <div
+                            className='navbar__menu-hamburger-container'
+                            onClick={() => setOpenMenu(true)}
+                        >
+                            <div className='navbar__menu-hamburger'></div>
+                        </div>
+
+                        <div className='navbar__menu--large'>
+                            <Menu />
+                        </div>
+                    </div>
+
+                    <div className='navbar__menu--large'>
+                        <Sign />
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Navbar;
